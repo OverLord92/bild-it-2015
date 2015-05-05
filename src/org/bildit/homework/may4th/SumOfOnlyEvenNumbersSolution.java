cdd9cbbeb36769e2a1170d9642cb3ddbb0b00bec
Branislav Lazic <brano2411@hotmail.com>
2015-05-05 20:30:39 +0200

    Added solutions for may4th

---
+++ src/org/bildit/homework/may4th/SumOfOnlyEvenNumbersSolution.java
@@ -0,0 +1,20 @@
+package org.bildit.homework.may4th;
+
+import java.util.Scanner;
+
+public class SumOfOnlyEvenNumbersSolution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Insert a number:");
+			int num = sc.nextInt();
+			if (num % 2 == 0) {
+				sum += num;
+				System.out.println("Sum:" + sum);
+			}
+		}
+		sc.close();
+	}
+}
---
+++ src/org/bildit/homework/may4th/SumSolution.java
@@ -0,0 +1,19 @@
+package org.bildit.homework.may4th;
+
+import java.util.Scanner;
+
+public class SumSolution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Insert a number:");
+			int num = sc.nextInt();
+			sum += num;
+			System.out.println("Sum:" + sum);
+		}
+		sc.close();
+	}
+}
